b3ff0205a1ffaee8222d89ee206e13e84b94b2a8
James Lance <james.lance@coxautoinc.com>
2021-02-06 21:23:55 -0500

    ran terraform fmt on main

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ resource "google_cloud_run_service" "app1" {
         # image = "gcr.io/cloudrun/hello"
         image = var.docker_image
         resources {
-          limits = {"memory"="128Mi"}
+          limits = { "memory" = "128Mi" }
         }
       }
     }
@@ -48,20 +48,20 @@ resource "google_monitoring_notification_channel" "basic" {
 }
 
 resource "google_monitoring_alert_policy" "alert_policy" {
-  display_name = "Denial of Money"
-  combiner     = "OR"
+  display_name          = "Denial of Money"
+  combiner              = "OR"
   notification_channels = [google_monitoring_notification_channel.basic.name]
   conditions {
     display_name = "condition 1"
     condition_threshold {
-      filter = "metric.type=\"run.googleapis.com/container/billable_instance_time\" resource.type=\"cloud_run_revision\" resource.label.\"service_name\"=\"app1\" resource.label.\"project_id\"=\"${var.project_id}\" resource.label.\"location\"=\"${var.region}\""
+      filter     = "metric.type=\"run.googleapis.com/container/billable_instance_time\" resource.type=\"cloud_run_revision\" resource.label.\"service_name\"=\"app1\" resource.label.\"project_id\"=\"${var.project_id}\" resource.label.\"location\"=\"${var.region}\""
       duration   = "0s"
       comparison = "COMPARISON_GT"
       aggregations {
-        alignment_period   = "60s"
+        alignment_period     = "60s"
         cross_series_reducer = "REDUCE_SUM"
-        per_series_aligner = "ALIGN_RATE"
-        group_by_fields = [ "resource.label.revision_name" ]
+        per_series_aligner   = "ALIGN_RATE"
+        group_by_fields      = ["resource.label.revision_name"]
       }
       trigger {
         count = 1
